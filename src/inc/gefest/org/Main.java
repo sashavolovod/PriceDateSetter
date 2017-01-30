b8f41c96055ff889f600c6db3ecc4edc6a0c7921
sasha volovod <sasha.volovod@gmail.com>
2017-01-30 21:06:30 +0300

    add Person class

--- src/inc/gefest/org/Main.java
+++ src/inc/gefest/org/Main.java
@@ -1,12 +1,16 @@
-/**
+/*
  * Created by Sasha Volovod on 30.01.17.
  * Copyright (c) 2017 Sasha Volovod. All rights reserved.
  * Licensed under the GNU GENERAL PUBLIC LICENSE, Version 3.
  * You may obtain a copy of the License at https://www.gnu.org/licenses/gpl-3.0.html
  */
+package inc.gefest.org;
 
 public class Main {
+
     public static void main(String[] args) {
-        System.out.printf("test pro");
+
+        Person p = new Person("Ivanov", 23);
+        System.out.printf(p.toString());
     }
 }
