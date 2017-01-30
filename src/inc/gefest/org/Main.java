fde79072e4ca4518c4638730bbba524457258cbd
sasha volovod <sasha.volovod@gmail.com>
2017-01-30 23:04:20 +0300

    add logging

--- src/inc/gefest/org/Main.java
+++ src/inc/gefest/org/Main.java
@@ -6,9 +6,16 @@
  */
 package inc.gefest.org;
 
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Logger;
+
 public class Main {
 
+    private static final Logger logger = Logger.getLogger(Main.class);
+
     public static void main(String[] args) {
+        logger.debug("Hello world.");
+        logger.info("What a beatiful day.");
 
         Person p = new Person("Ivanov", 23);
         System.out.printf(p.toString());
