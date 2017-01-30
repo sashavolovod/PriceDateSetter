46f011ceb74423637561ce9ed9978a6abc1eb701
sashavolovod <sasha.volovod@gmail.com>
2017-01-30 14:42:55 +0300

    add

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
 /**
- * Created by udly on 30.01.17.
- * 
+ * Created by Sasha Volovod on 30.01.17.
+ * Copyright (c) 2017 Sasha Volovod. All rights reserved.
+ * Licensed under the GNU GENERAL PUBLIC LICENSE, Version 3.
+ * You may obtain a copy of the License at https://www.gnu.org/licenses/gpl-3.0.html
  */
+
 public class Main {
     public static void main(String[] args) {
         System.out.printf("test pro");
